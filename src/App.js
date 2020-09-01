7a09d47b57b7096f8196beb769c95fbf35648193
aarunbala <aarunbala@gmail.com>
2020-09-01 16:55:57 +1000

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React @ Rapid pace
         </a>
       </header>
     </div>
